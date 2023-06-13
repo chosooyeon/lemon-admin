f4104256aaac3dd0b137784d1a08bcf4efc081b4
sungaee <sungaee121@naver.com>
2023-06-13 15:33:15 +0900

    💄 vite 머터리얼UI 환경설정 추가

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -1,7 +1,15 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 const MenuPage = () => {
     return(
         <>
             <div>메뉴페이지</div>
+             <Stack spacing={2} direction="row">
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+            </Stack>
         </>
     )
 }
