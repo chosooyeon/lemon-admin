f406dd237e1b0e8683d658688a1e90503f2f50d2
sungaee <sungaee121@naver.com>
2023-06-15 18:03:24 +0900

    로그인 react-hook-form 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,68 @@
+import { useForm } from "react-hook-form";
+import { Button, TextField, Box } from '@mui/material/';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+
 const LoginPage = () => {
-    return(
+    const {
+        register,
+        handleSubmit,
+    } = useForm();
+
+    const submitForm = (data) => {
+        console.log(data);
+        console.log('3333')
+    };
+
+    return (
         <>
-            <div>로그인페이지</div>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+                >
+                <div>Sign to Account</div>
+                <form autoComplete="off" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        required
+                        autoFocus
+                        fullWidth
+                        type="email"
+                        id="email"
+                        label="Username"
+                        autoComplete="email"
+                        {...register("email", { required: true, maxLength: 10 })}
+                    />
+                    <TextField
+                        required
+                        autoFocus
+                        fullWidth
+                        type="password"
+                        id="password"
+                        label="Password"
+                        autoComplete="current-password"
+                        {...register("password", { required: true, maxLength: 10 })}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        size="large"
+                    >
+                        Sign in
+                    </Button>
+                </form>
+            </Box>
         </>
     )
 }
-
 export default LoginPage
