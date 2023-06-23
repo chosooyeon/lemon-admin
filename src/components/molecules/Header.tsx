f416522eb7a6e32489160f1d672dc5595113e6fb
sungaee <sungaee121@naver.com>
2023-06-23 15:35:28 +0900

    🎨 로그아웃 버튼 Link연결

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,20 +1,28 @@
 import hamburger from '@/assets/hamburger.png';
 import { useSetRecoilState } from 'recoil';
 import { sideBar } from '@/recoil';
+import { Box, Button } from '@mui/joy';
+import { Link } from "react-router-dom";
+import LogoutIcon from '@mui/icons-material/Logout';
+
 
 const Header = () => {
     const setToggleAtom = useSetRecoilState(sideBar);
     const toggle = () => setToggleAtom((prev) => !prev);
 
-    return(
+    return (
         <>
             <section className='flex justify-between px-6 h-[60px] items-center shadow-lg shadow-slate-100'>
                 <section className='flex space-x-2 items-center'>
-                    <img width={30} src={hamburger} onClick={toggle}/>
+                    <img width={30} src={hamburger} onClick={toggle} />
                     <div>React Admin</div>
                 </section>
                 <section>
-                    <div>로그아웃</div>
+                    <Link to="/">
+                        <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+                            <Button startDecorator={<LogoutIcon />} variant="soft">로그아웃</Button>
+                        </Box>
+                    </Link>
                 </section>
             </section>
         </>
